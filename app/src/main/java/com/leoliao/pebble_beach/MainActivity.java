31c271427a776632f155b46fb7ce7d86e324722c
leoliao2008@163.com <leoliao2008@163.com>
2018-03-19 01:45:11 +0800

    first commit.

--- app/src/main/java/com/leoliao/pebble_beach/MainActivity.java
+++ app/src/main/java/com/leoliao/pebble_beach/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
 //        // Example of a call to a native method
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
 //        tv.setText(stringFromJNI());
