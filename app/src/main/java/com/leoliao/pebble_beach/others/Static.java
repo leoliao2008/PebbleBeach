ebf2fa9895f06c341b5ad00188217e76219eb8e6
leoliao2008@163.com <leoliao2008@163.com>
2018-03-19 02:11:15 +0800

    add bmob dependencies

---
+++ app/src/main/java/com/leoliao/pebble_beach/others/Static.java
@@ -0,0 +1,15 @@
+package com.leoliao.pebble_beach.others;
+
+/**
+ * 创建者     $Author$
+ * 创建时间   2018/3/19 1:58
+ * 描述	      ${TODO}
+ * <p>
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述   ${TODO}
+ */
+
+public interface Static {
+    String APP_ID="417c9cd8780777d8c6d5e48a7b8edfd8";
+}
